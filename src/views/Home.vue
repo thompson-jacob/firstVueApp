38e17f1c4ca646cc95ae9a1f1cea2f72bd52ad26
Jacob Thompson <Cobalt7291@gmail.com>
2021-02-09 19:39:02 -0500

    single page vue.js app w/axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <h1>New Recipe</h1>
+    Name:
+    <input type="text" v-model="newProductName" />
+    Price:
+    <input type="text" v-model="newProductPrice" />
+    Description
+    <input type="text" v-model="newProductDescription" />
+    Image Url:
+    <input type="text" v-model="newProductImageUrl" />
+    <button v-on:click="createProduct()">Make New Product</button>
+
+    <div v-for="product in products" v-bind:key="product.id">
+      {{ product }}
+      <h2>Name: {{ product.name }}</h2>
+      <p>Price: {{ product.price }}</p>
+      <p>Description: {{ product.description }}</p>
+      <img v-bind:src="product.image_url" alt="product image" />
+      <div>
+        <button v-on:click="showProduct(product)">More info</button>
+      </div>
+    </div>
+
+    <dialog id="product-deatils">
+      <form method="dialog">
+        <h1>Product Info</h1>
+        <p>Title:...</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      message: "Here are some products for you to peruse!",
+      products: [],
+      newProductName: "",
+      newProductPrice: "",
+      newProductDescription: "",
+      newProductImageUrl: "",
+    };
+  },
+  created: function() {
+    this.indexProducts();
+  },
+  methods: {
+    // Create rest of CRUD app
+    indexProducts: function() {
+      axios.get("/api/products").then(response => {
+        this.products = response.data;
+        console.log("All Products:", this.products);
+      });
+    },
+    createProduct: function() {
+      var params = {
+        name: this.newProductName,
+        price: this.newProductPrice,
+        description: this.newProductDescription,
+        imageUrl: this.newProductImageUrl,
+      };
+      axios
+        .post("/api/products", params)
+        .then(response => {
+          console.log("Great Success!", response.data);
+          this.products.push(response.data);
+        })
+        .catch(error => console.log(error.response));
+    },
+    showProduct: function(product) {
+      console.log("showProduct", product.title, product);
+      document.querySelector("#product-details").showModal();
+    },
   },
 };
 </script>
